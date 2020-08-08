4219b4c506e0e2ed1f0b4b93bbfc1d0d38d87f1d
Ahmed Ramadan <medoandahmed783@gmail.com>
2020-08-07 22:24:13 -0400

    ENVIR UPDATE

--- Environment.java
+++ Environment.java
@@ -5,7 +5,32 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.util.*;
 public class Environment
 {
-    public
+    private String virus;
+    private int size;
+    private int infected;
+    private int potato;
+    
+    public Environment(int size)
+    {
+    this.size = size;
+    virus = "Covid19";
+    infected = 0;
+    infected = (int) (Math.random() * 10 + 1);
+    }
+    public Environment(int size, String virus)
+    {
+    this.size = size;
+    this.virus = virus;
+    infected = 0;
+    infected = (int) (Math.random() * 10 + 1);
+    }
+    public Environment(int size, String virus, int infected)
+    {
+    this.size = size;
+    this.virus = virus;
+    this.infected = infected;
+    }
 }
