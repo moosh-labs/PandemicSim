4251c1543b98d36d89409639e5b589b43c2dc996
Hazem Zaky <hazaky@ctemc.org>
2020-08-07 21:19:02 -0400

    heh

---
+++ Person.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Person here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Person
+{
+    
+    
+    public Person()
+    {
+        
+    }
+    
+    
+}
